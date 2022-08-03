5149b1a3174ce1d99c865089aee2a8efdea3bb36
Heewon Ham <78684994+heewonham@users.noreply.github.com>
2022-08-03 23:51:11 +0900

    [Gold V] Title: 무기 공학, Time: 464 ms, Memory: 81144 KB -BaekjoonHub

--- 백준/Gold/18430. 무기 공학/무기 공학.java
+++ 백준/Gold/18430. 무기 공학/무기 공학.java
@@ -7,6 +7,14 @@ public class Main {
     static int n, m, max = 0;
     static int[] wingR1 = {0,0,0,0}, wingC1 = {1,-1,1,-1};
     static int[] wingR2 = {1,1,-1,-1}, wingC2 = {0,0,0,0};
+    static class Pos{
+        int r, c;
+
+        public Pos(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -24,10 +32,10 @@ public class Main {
 
         int [][][] power = new int[n][m][4];
         boolean[][] visited = new boolean[n][m];
-        bfs(visited, power, map,0,0,0);
+        bfs(visited, power, map,0,0);
         System.out.println(max);
     }
-    static void bfs(boolean[][] visited, int[][][] power, int[][] map, int r, int c, int sum){
+    static void bfs(boolean[][] visited, int[][][] power, int[][] map, int r,int sum){
 
         if(sum > max) max = sum;
 
@@ -35,34 +43,29 @@ public class Main {
             for(int j = 0; j < m; j++){
                 if(visited[i][j]) continue;
                 for(int k =0 ; k < 4; k++){
-                    if(isPossible(visited, i,j,k)){
-                        makeVisited(visited, i, j, k, true);
-                        bfs(visited, power, map, i,j, sum+makePower(power, map, i,j,k));
-                        makeVisited(visited, i, j, k, false);
+                    Pos pos1 = new Pos(i + wingR1[k],j + wingC1[k]);
+                    Pos pos2 = new Pos(i + wingR2[k], j + wingC2[k]);
+                    if(isPossible(visited, pos1,pos2)){
+                        makeVisited(visited, i, j, pos1, pos2, true);
+                        bfs(visited, power, map, i, sum+makePower(power, map, i, j, pos1, pos2, k));
+                        makeVisited(visited, i, j, pos1, pos2,false);
                     }
                 }
             }
         }
     }
-    static boolean isPossible(boolean[][] visited, int r, int c, int dir){
-        int r1 = r + wingR1[dir], c1 = c + wingC1[dir];
-        int r2 = r + wingR2[dir], c2 = c + wingC2[dir];
-        if(r1 < 0 || r1 >= n || c1 < 0 || c1 >= m) return false;
-        if(r2 < 0 || r2 >= n || c2 < 0 || c2 >= m) return false;
-        return !visited[r1][c1] && !visited[r2][c2];
+    static boolean isPossible(boolean[][] visited, Pos p1, Pos p2){
+        if(p1.r < 0 || p1.r >= n || p1.c < 0 || p1.c >= m) return false;
+        if(p2.r < 0 || p2.r >= n || p2.c < 0 || p2.c >= m) return false;
+        return !visited[p1.r][p1.c] && !visited[p2.r][p2.c];
     }
-    static void makeVisited(boolean[][] visited, int r, int c, int dir, boolean bool){
-        int r1 = r + wingR1[dir], c1 = c + wingC1[dir];
-        int r2 = r + wingR2[dir], c2 = c + wingC2[dir];
-        visited[r1][c1] = bool;
+    static void makeVisited(boolean[][] visited, int r, int c, Pos p1, Pos p2, boolean bool){
+        visited[p1.r][p1.c] = bool;
         visited[r][c] = bool;
-        visited[r2][c2] = bool;
+        visited[p2.r][p2.c] = bool;
     }
-    static int makePower(int[][][] power, int[][] map, int r, int c, int dir){
+    static int makePower(int[][][] power, int[][] map, int r, int c, Pos p1, Pos p2, int dir){
         if(power[r][c][dir] != 0) return power[r][c][dir];
-        int r1 = r + wingR1[dir], c1 = c + wingC1[dir];
-        int r2 = r + wingR2[dir], c2 = c + wingC2[dir];
-
-        return power[r][c][dir] = map[r1][c1] + map[r2][c2] + (map[r][c] * 2);
+        return power[r][c][dir] = map[p1.r][p1.c] + map[p2.r][p2.c] + (map[r][c] * 2);
     }
 }
