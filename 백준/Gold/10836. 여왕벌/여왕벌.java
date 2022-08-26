c17444d9995a415efe3bca9cf1dd796ac338e951
Heewon Ham <78684994+heewonham@users.noreply.github.com>
2022-08-26 19:28:32 +0900

    [Gold IV] Title: 여왕벌, Time: 1480 ms, Memory: 284984 KB -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.java
+++ 백준/Gold/10836. 여왕벌/여왕벌.java
@@ -12,70 +12,37 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int[][] map = new int[n][n];
-        plusZero(map, n);
-        for(int i = 0; i < m; i++){
+        int[] map = new int[n+n-1];
+        init(map);
+        for(int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
 
             int zero = Integer.parseInt(st.nextToken());
             int one = Integer.parseInt(st.nextToken());
-            int two = Integer.parseInt(st.nextToken());
+            Integer.parseInt(st.nextToken());
 
-            for(int j = n-1; j >= 0; j--){
-                if(zero != 0) zero--;
-                else if(one != 0){
-                    one--;
-                    map[j][0] += 1;
-                }else if(two != 0){
-                    two--;
-                    map[j][0] += 2;
-                }
+            for (int j = zero; j < zero + one; j++) {
+                map[j]++;
             }
-            for(int j = 1; j < n; j++){
-                if(zero != 0) zero--;
-                else if(one != 0){
-                    one--;
-                    map[0][j] += 1;
-                }else if(two != 0){
-                    two--;
-                    map[0][j] += 2;
-                }
+            for (int j = zero+one; j < map.length; j++) {
+                map[j] += 2;
             }
         }
-
-        // find
-        plusSecond(map,n);
 
         // result
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                sb.append(map[i][j] + " ");
+            sb.append(map[n-i-1] + " ");
+            for(int j = 1; j < n; j++){
+                sb.append(map[n+j-1] + " ");
             }
             sb.append("\n");
         }
         System.out.println(sb);
     }
-    static void plusZero(int[][] map, int n){
-        for(int i = n-1; i >= 0; i--){
-            map[i][0] += 1;
+    static void init(int[] map){
+        for(int i = 0; i < map.length; i++){
+            map[i] = 1;
         }
-        for(int i = 1; i < n; i++){
-            map[0][i] += 1;
-        }
-    }
-    static void plusSecond(int[][] map, int n){
-        for(int i = 1; i < n; i++){
-            for(int j = 1; j < n; j++){
-                int a = map[i+mr[0]][j+mc[0]];
-                int b = map[i+mr[1]][j+mc[1]];
-                int c = map[i+mr[2]][j+mc[2]];
-                map[i][j] = findMax(a,b,c);
-            }
-        }
-    }
-    static int findMax(int a, int b, int c){
-        int ab = Math.max(a,b);
-        return Math.max(ab, c);
     }
 }
