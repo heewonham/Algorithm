8cdb2d82010c5bbd745a7b8862d2c54f93ba39c9
Heewon Ham <78684994+heewonham@users.noreply.github.com>
2022-09-01 21:08:58 +0900

    [Bronze II] Title: 스페셜 저지, Time: 2144 ms, Memory: 17892 KB -BaekjoonHub

---
+++ 백준/Bronze/9253. 스페셜 저지/스페셜 저지.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        String s3 = br.readLine();
+
+        if(s1.contains(s3) && s2.contains(s2)) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
