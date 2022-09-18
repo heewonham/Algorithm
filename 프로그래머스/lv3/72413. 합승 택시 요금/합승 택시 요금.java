f04168ad99490a5cca7e3ab36c856ca87763c2a9
Heewon Ham <78684994+heewonham@users.noreply.github.com>
2022-09-18 14:12:20 +0900

    [level 3] Title: 합승 택시 요금, Time: 21.18 ms, Memory: 53.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+class Solution {
+    private static final int MAX = 20000005;
+    class Node{
+        int vertex, weight;
+
+        public Node(int vertex, int weight) {
+            this.vertex = vertex;
+            this.weight = weight;
+        }
+    }
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        
+         List<Node>[] connection = new List[n+1];
+        for(int i = 1; i <= n; i++) connection[i] = new ArrayList<>();
+        for(int[] fare : fares){
+            connection[fare[0]].add(new Node(fare[1], fare[2]));
+            connection[fare[1]].add(new Node(fare[0], fare[2]));
+        }
+
+        
+        /***
+         * 각자 최단 거리 더한 값과
+         * 일정한 노드까지 같이간 경로 더허가 비교
+         */
+        int answer = findShortestPath(connection, n, s, a) + findShortestPath(connection, n, s, b);
+
+        for(int i = 1 ; i <= n; i++){
+            if(s == i) continue;
+            answer = Math.min(answer, (findShortestPath(connection, n, s, i) +
+                                       findShortestPath(connection, n, i, a) +
+                                       findShortestPath(connection, n, i, b)));
+        }
+
+        return answer;
+    }
+    int findShortestPath(List<Node>[] connection, int n, int s, int e){
+        int[] dist = new int[n+1];
+        for(int i = 1; i <= n; i++) dist[i] = MAX;
+        dist[s] = 0;
+
+        PriorityQueue<Node> pq = new PriorityQueue<>(
+            (o1, o2) -> {return o1.weight - o2.weight;});
+        pq.add(new Node(s, 0));
+
+        while(!pq.isEmpty()){
+            Node current = pq.poll();
+            int node = current.vertex;
+            int weight = current.weight;
+            
+            if(node == e) return weight;
+
+            for(int i = 0; i < connection[node].size(); i++){
+                Node next = connection[node].get(i);
+                if(dist[next.vertex] > weight + next.weight){
+                    dist[next.vertex] = weight + next.weight;
+                    pq.add(new Node(next.vertex, dist[next.vertex]));
+                }
+            }
+        }
+        return dist[e];
+    }
+}
